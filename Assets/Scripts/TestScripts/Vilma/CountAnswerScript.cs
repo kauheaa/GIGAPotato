0461c3056536ba5ff947b261c9f2a907b5add991
kauheaa <oksanen.vilma@gmail.com>
2021-12-15 14:55:45 +0200

    CountScripts duplicated for testing, Counting levels in building
    - QuizManager duplicate is named "CountScript"
    - AnswerScript duplicate is named "CountAnswerScript"
    - LevelEnd, Stickers and reset function added to script duplicates

---
+++ Assets/Scripts/TestScripts/Vilma/CountAnswerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountAnswerScript : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public CountScript quizManager;
+
+    public Color startColor;
+
+    private void Start()
+    {
+        startColor = GetComponent<Image>().color;
+    }
+
+    public void Answer()
+    {
+        if(isCorrect)
+        {
+            GetComponent<Image>().color = Color.green;
+            Debug.Log("Correct Answer");
+            quizManager.correct();
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.red;
+            Debug.Log("Wrong Answer");
+            quizManager.wrong();
+        }
+    }
+
+}
