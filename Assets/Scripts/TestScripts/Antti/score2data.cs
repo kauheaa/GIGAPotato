0ffd1b49c27ba566859570cf831425c1fc3dd9af
Danteziii <antti.lahtinen@edu.taitotalo.fi>
2021-12-17 13:33:34 +0200

    Substract score added

---
+++ Assets/Scripts/TestScripts/Antti/score2data.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class score2data
+
+
+{
+    public int subScore;
+
+    public score2data(SubScript sub)
+    {
+
+        subScore = sub.subScore;
+
+    }
+
+    }
