29b5a3207e655520d78f21af9d22eabc7ff8408b
Danteziii <antti.lahtinen@edu.taitotalo.fi>
2022-01-04 10:43:05 +0200

    Tehty tallennusmuutokset divide ja multiply scriptiin

--- Assets/Scripts/TestScripts/Antti/DivideScript.cs
+++ Assets/Scripts/TestScripts/Antti/DivideScript.cs
@@ -6,82 +6,372 @@ using UnityEngine.UI;
 public class DivideScript : MonoBehaviour
 {
     int firstValue, secondValue, tempValue, finalValue, Alternative1, Alternative2;
-    [SerializeField] public int sumScore = 0;
-    [SerializeField] public int subScore = 0;
     [SerializeField] public int divScore = 0;
-    [SerializeField] public int multScore = 0;
-    [SerializeField] private Transform switchOff, switchOn;
+    [SerializeField] public int sumStarCount = 0;
+    [SerializeField] public int appleStickerScore = 0;
+    [SerializeField] public int basketStickerScore = 0;
+    [SerializeField] public int pigStickerScore = 0;
+    public int levelIndex = 0;
+    private int stick1, stick2, stick3;
     public Text FirstValue, SecondValue, Function, Alt1, Alt2, Alt3, AnswerSpot, scoreCount;
-    public GameObject ONE, TWO, THREE, appleSpawn, apple, stickerOne, stickerTwo, stickerThree, star;
+    public GameObject ONE, TWO, THREE, appleSpawn, apple, stickerOne, stickerTwo, stickerThree;
+    public Sprite oneStar, twoStar, threeStar;
+    public GameObject sumStars, menuStars;
     public Button button1, button2, button3;
-    SumScript sum;
-    MultiplyScript mult;
-    SubScript sub;
+    public Sprite blueButton, redButton, greenButton;
+    [SerializeField] private Transform switchOff, switchOn;
 
 
+    public void SaveScore()
+    {
+        saveScore.SaveDivScore(this);
+
+        //luo väliaikaisen listan, joka etsii hierarkiassa olevat sumScore instanssit ja käy läpi,
+        //käy läpi kaikki löytämänsä instanssit ja käskee niitä hakemaan tietokannasta kaikki tallennetut arvot;
+        //näin kaikissa sumScore-instansseissa näkyy kaikkien tarrojen "StickerScore" jolloin seuraava tallennus
+        //ei ylikirjoita arvoja nollaksi 
+        DivideScript[] tempArray = GameObject.FindObjectsOfType<DivideScript>();
+        foreach (DivideScript i in tempArray)
+        {
+            i.LoadScore();
+        }
+
+    }
+    public void LoadScore()
+    {
+        scoreData data = saveScore.LoadDivScore();
+        sumStarCount = data.sumStarCount;
+        appleStickerScore = data.appleStickerScore;
+        basketStickerScore = data.basketStickerScore;
+        pigStickerScore = data.pigStickerScore;
+        Debug.Log("apple: " + appleStickerScore + " basket: " + basketStickerScore + " pig: " + pigStickerScore);
+
+    }
+
+    public void SetLevelIndex(int index)
+    {
+        levelIndex = index;
+    }
+
+    public void ResetScore()
+    {
+        divScore = 0;
+        scoreCount.text = divScore.ToString();
+    }
 
     private void Start()
     {
+        scoreCount.text = divScore.ToString();
         AnswerSpot.text = "?";
-        sumScore = sum.sumScore;
-        subScore = sub.subScore;
-        multScore = mult.divScore;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //   score = int.Parse(scoreCount.text);
+       
 
-        if (divScore >= 5)
+        sumStarCount = appleStickerScore + basketStickerScore + pigStickerScore;
+
+        if (appleStickerScore == 1)
         {
             stickerOne.gameObject.SetActive(true);
-            star.gameObject.SetActive(true);
         }
-        if (divScore >= 10)
+        if (basketStickerScore == 1)
         {
             stickerTwo.gameObject.SetActive(true);
-
         }
-        if (divScore >= 15)
+        if (pigStickerScore == 1)
         {
             stickerThree.gameObject.SetActive(true);
-
+        }
+        if (sumStarCount == 1)
+        {
+            sumStars.GetComponent<Image>().sprite = oneStar;
+            menuStars.GetComponent<Image>().sprite = oneStar;
+        }
+        if (sumStarCount == 2)
+        {
+            sumStars.GetComponent<Image>().sprite = twoStar;
+            menuStars.GetComponent<Image>().sprite = twoStar;
+        }
+        if (sumStarCount == 3)
+        {
+            sumStars.GetComponent<Image>().sprite = threeStar;
+            menuStars.GetComponent<Image>().sprite = threeStar;
         }
     }
 
+    
+        public void DivJungle()
+        {
 
-    public void DivFarm()
+            firstValue = Random.Range(2, 10);
+            secondValue = Random.Range(2, 10);
+            FirstValue.text = firstValue.ToString();
+            SecondValue.text = secondValue.ToString();
+
+            if (firstValue - secondValue < 2)
+            {
+                tempValue = secondValue;
+                secondValue = firstValue;
+                firstValue = tempValue;
+            }
+
+            Function.text = "/";
+            finalValue = firstValue / secondValue;
+
+
+            tempValue = Random.Range(2, 20);
+            while (tempValue == finalValue)
+            {
+                tempValue = Random.Range(2, 20);
+            }
+            Alternative1 = tempValue;
+
+            //Second Alternative
+            tempValue = Random.Range(2, 20);
+            while (tempValue == finalValue || (tempValue == Alternative1))
+            {
+                tempValue = Random.Range(2, 20);
+            }
+            Alternative2 = tempValue;
+
+            tempValue = Random.Range(1, 6);
+            if (tempValue == 1)
+            {
+                Alt1.text = finalValue.ToString(); Alt2.text = Alternative1.ToString(); Alt3.text = Alternative2.ToString();
+            }
+            if (tempValue == 2)
+            {
+                Alt1.text = finalValue.ToString(); Alt2.text = Alternative2.ToString(); Alt3.text = Alternative1.ToString();
+            }
+            if (tempValue == 3)
+            {
+                Alt1.text = Alternative1.ToString(); Alt2.text = finalValue.ToString(); Alt3.text = Alternative2.ToString();
+            }
+            if (tempValue == 4)
+            {
+                Alt1.text = Alternative1.ToString(); Alt2.text = Alternative2.ToString(); Alt3.text = finalValue.ToString();
+            }
+            if (tempValue == 5)
+            {
+                Alt1.text = Alternative2.ToString(); Alt2.text = finalValue.ToString(); Alt3.text = Alternative1.ToString();
+            }
+            if (tempValue == 6)
+            {
+                Alt1.text = Alternative2.ToString(); Alt2.text = Alternative1.ToString(); Alt3.text = finalValue.ToString();
+            }
+
+
+
+
+
+            Debug.Log(firstValue + "  FUNCTION  " + secondValue + "=" + finalValue);
+        }
+
+    
+
+    public void AltOne()
+    {
+        if (Alt1.text == finalValue.ToString())
+        {
+            ONE.gameObject.SetActive(true);
+            button1.interactable = false;
+            button2.interactable = false;
+            button3.interactable = false;
+            ONE.GetComponent<Image>().sprite = greenButton;
+            StartCoroutine(Correct());
+        }
+        if (Alt1.text != finalValue.ToString())
+        {
+            ONE.gameObject.SetActive(true);
+            button1.interactable = false;
+            ONE.GetComponent<Image>().sprite = redButton;
+        }
+    }
+
+    public void AltTwo()
     {
 
-        firstValue = Random.Range(2, 10);
-        secondValue = Random.Range(2, 10);
+        if (Alt2.text == finalValue.ToString())
+        {
+            TWO.gameObject.SetActive(true);
+            button1.interactable = false;
+            button2.interactable = false;
+            button3.interactable = false;
+            TWO.GetComponent<Image>().sprite = greenButton;
+            StartCoroutine(Correct());
+        }
+        if (Alt2.text != finalValue.ToString())
+        {
+            TWO.gameObject.SetActive(true);
+            button2.interactable = false;
+            TWO.GetComponent<Image>().sprite = redButton;
+        }
+    }
+
+    public void AltThree()
+    {
+        if (Alt3.text == finalValue.ToString())
+        {
+            THREE.gameObject.SetActive(true);
+            button1.interactable = false;
+            button2.interactable = false;
+            button3.interactable = false;
+            THREE.GetComponent<Image>().sprite = greenButton;
+            StartCoroutine(Correct());
+        }
+        if (Alt3.text != finalValue.ToString())
+        {
+            THREE.gameObject.SetActive(true);
+            button3.interactable = false;
+            THREE.GetComponent<Image>().sprite = redButton;
+        }
+    }
+
+    
+
+    public void ResetJungle()
+    {
+        {
+            if (divScore >= 5)
+            {
+                switchOn.gameObject.SetActive(true);
+                switchOff.gameObject.SetActive(false);
+                switch (levelIndex)
+                {
+                    case 1:
+                        if (appleStickerScore < 1)
+                        {
+                            appleStickerScore += 1;
+                            Debug.Log("Apple unlocked");
+                        }
+                        break;
+                    case 2:
+                        if (basketStickerScore < 1)
+                        {
+                            basketStickerScore += 1;
+                            Debug.Log("Basket unlocked");
+                        }
+                        break;
+                    case 3:
+                        if (pigStickerScore < 1)
+                        {
+                            pigStickerScore += 1;
+                            Debug.Log("Pig unlocked");
+                        }
+                        break;
+                    default:
+                        Debug.Log("No level index set");
+                        break;
+                }
+
+            }
+
+            ONE.gameObject.SetActive(false);
+            button1.interactable = true;
+            TWO.gameObject.SetActive(false);
+            button2.interactable = true;
+            THREE.gameObject.SetActive(false);
+            button3.interactable = true;
+            AnswerSpot.text = "?";
+            DivJungle();
+        }
+    }
+    public void ResetSpace()
+    {
+        {
+            if (divScore >= 5)
+            {
+                switchOn.gameObject.SetActive(true);
+                switchOff.gameObject.SetActive(false);
+                switch (levelIndex)
+                {
+                    case 1:
+                        if (appleStickerScore < 1)
+                        {
+                            appleStickerScore += 1;
+                            Debug.Log("Apple unlocked");
+                        }
+                        break;
+                    case 2:
+                        if (basketStickerScore < 1)
+                        {
+                            basketStickerScore += 1;
+                            Debug.Log("Basket unlocked");
+                        }
+                        break;
+                    case 3:
+                        if (pigStickerScore < 1)
+                        {
+                            pigStickerScore += 1;
+                            Debug.Log("Pig unlocked");
+                        }
+                        break;
+                    default:
+                        Debug.Log("No level index set");
+                        break;
+                }
+
+            }
+
+            ONE.gameObject.SetActive(false);
+            button1.interactable = true;
+            TWO.gameObject.SetActive(false);
+            button2.interactable = true;
+            THREE.gameObject.SetActive(false);
+            button3.interactable = true;
+            AnswerSpot.text = "?";
+            DivSpace();
+        }
+    }
+
+    IEnumerator Correct()
+    {
+        Score();
+        AnswerSpot.text = finalValue.ToString();
+        scoreCount.text = divScore.ToString();
+        yield return new WaitForSeconds(1f);
+        ResetJungle();
+    }
+
+    public void Score()
+    {
+        divScore += 1;
+    }
+
+   
+    public void DivSpace()
+    {
+        firstValue = Random.Range(1, 1000);
+        secondValue = Random.Range(1, 1000);
         FirstValue.text = firstValue.ToString();
         SecondValue.text = secondValue.ToString();
 
-        if (firstValue - secondValue < 2)
+        if (firstValue - secondValue < 0)
         {
             tempValue = secondValue;
             secondValue = firstValue;
             firstValue = tempValue;
         }
 
-        Function.text = "/";
-        finalValue = firstValue / secondValue;
+        Function.text = "+";
+        finalValue = firstValue + secondValue;
 
 
-        tempValue = Random.Range(2, 20);
+        tempValue = Random.Range(50, 1900);
         while (tempValue == finalValue)
         {
-            tempValue = Random.Range(2, 20);
+            tempValue = Random.Range(50, 1900);
         }
         Alternative1 = tempValue;
 
         //Second Alternative
-        tempValue = Random.Range(2, 20);
+        tempValue = Random.Range(50, 1900);
         while (tempValue == finalValue || (tempValue == Alternative1))
         {
-            tempValue = Random.Range(2, 20);
+            tempValue = Random.Range(50, 1900);
         }
         Alternative2 = tempValue;
 
@@ -116,149 +406,24 @@ public class DivideScript : MonoBehaviour
 
 
         Debug.Log(firstValue + "  FUNCTION  " + secondValue + "=" + finalValue);
-    }
-    public void AltOne()
-    {
-        if (Alt1.text == finalValue.ToString())
-        {
-            ONE.gameObject.SetActive(true);
-            button1.interactable = false;
-            StartCoroutine(Correct());
-
-
-        }
-    }
-
-    public void AltTwo()
-    {
-
-        if (Alt2.text == finalValue.ToString())
-        {
-            TWO.gameObject.SetActive(true);
-            button2.interactable = false;
-            StartCoroutine(Correct());
-
-        }
-
-    }
-    public void AltThree()
-    {
-        if (Alt3.text == finalValue.ToString())
-        {
-            THREE.gameObject.SetActive(true);
-            button3.interactable = false;
-            StartCoroutine(Correct());
-
-
-        }
-    }
-    public void ResetV()
-    {
-        if (divScore == 5)
-        {
-            switchOn.gameObject.SetActive(true);
-            switchOff.gameObject.SetActive(false);
-        }
-        if (divScore == 10)
-        {
-            switchOn.gameObject.SetActive(true);
-            switchOff.gameObject.SetActive(false);
-        }
-        if (divScore == 15)
-        {
-            switchOn.gameObject.SetActive(true);
-            switchOff.gameObject.SetActive(false);
-        }
-        ONE.gameObject.SetActive(false);
-        button1.interactable = true;
-        TWO.gameObject.SetActive(false);
-        button2.interactable = true;
-        THREE.gameObject.SetActive(false);
-        button3.interactable = true;
-        AnswerSpot.text = "?";
-        DivFarm();
-    }
-    IEnumerator Correct()
-    {
-        Score();
-        AnswerSpot.text = finalValue.ToString();
-        scoreCount.text = divScore.ToString();
-        yield return new WaitForSeconds(1f);
-        ResetV();
-
-
-
-
-    }
-    void Score()
-    {
-        divScore += 1;
-    }
-
-    public void DivJungle()
-    {
-        firstValue = Random.Range(2, 100);
-        secondValue = Random.Range(2, 100);
-        FirstValue.text = firstValue.ToString();
-        SecondValue.text = secondValue.ToString();
-
-        if (firstValue - secondValue < 2)
-        {
-            tempValue = secondValue;
-            secondValue = firstValue;
-            firstValue = tempValue;
-        }
-
-        Function.text = "+";
-        finalValue = firstValue / secondValue;
-
-    }
-    public void DivSpace()
-    {
-        firstValue = Random.Range(2, 1000);
-        secondValue = Random.Range(2, 1000);
-        FirstValue.text = firstValue.ToString();
-        SecondValue.text = secondValue.ToString();
-
-        if (firstValue - secondValue < 2)
-        {
-            tempValue = secondValue;
-            secondValue = firstValue;
-            firstValue = tempValue;
-        }
-
-        Function.text = "+";
-        finalValue = firstValue / secondValue;
 
     }
 
-    public void StartFarm()
-    {
-        ResetV();
-        DivFarm();
-    }
+  
+
     public void StartJungle()
     {
-        ResetV();
+        ResetJungle();
         DivJungle();
+        ResetScore();
     }
     public void StartSpace()
     {
-        ResetV();
+        ResetSpace();
         DivSpace();
+        ResetScore();
     }
- /*   public void SaveScore()
-    {
-        saveScore.SaveSumScore(sum);
-    }
-    public void LoadScore()
-    {
-        scoreData data = saveScore.LoadSumScore();
-        sumScore = data.sumScore;
-        subScore = data.subScore;
-        multScore = data.multScore;
-        divScore = data.divScore;
-    }
- */
+
 
 }
+
